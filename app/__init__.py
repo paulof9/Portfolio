a7165c0ccd91bf0fef2ca5d758212986228a0abd
paulof9 <above2@outlook.com>
2025-07-13 20:38:23 -0300

    chore: fix vercel deploy

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    # Importa e registra o blueprint principal
     from app.blueprints.main import main as main_bp
     app.register_blueprint(main_bp)
 
